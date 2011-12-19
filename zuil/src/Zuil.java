9db79fdac6de9d9c4be0098c8ee75503b1df1db9
Floris Maathuis <floris@florismaathuis.nl>
2011-12-19 14:22:08 +0100

    testverandering

--- zuil/src/Zuil.java
+++ zuil/src/Zuil.java
@@ -15,7 +15,7 @@ public class Zuil {
 	
 	public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
-        
+        System.out.println("Hello World!2");
         Zuil z = new Zuil();
         
         
